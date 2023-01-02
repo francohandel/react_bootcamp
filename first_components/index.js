f945e052e954ea8d2ddfb3a4af1e02a975ea2982
francohandel <francohandel@gmail.com>
2023-01-02 22:38:56 +0800

    first component

---
+++ first_components/index.js
@@ -0,0 +1,11 @@
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world...</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Hello/>, document.getElementById('root'))

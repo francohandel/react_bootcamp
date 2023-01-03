2dddea2724908d9af2c8bc6c7074ffc408309f92
francohandel <francohandel@gmail.com>
2023-01-03 23:50:26 +0800

    jsx conditionals

---
+++ conditionals/index.js
@@ -0,0 +1,15 @@
+class Guess extends React.Component {
+  render() {
+    const randNum = Math.floor(Math.random() * 10) + 1
+    let msg = <h1>Welcome to Number guessing game</h1>
+    return(
+      <div>
+        {msg}
+        <p>number guessed is {randNum}</p>
+        <p>{randNum === 7 && <span>congrats!! number guessed correctly</span>}</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Guess/>, document.getElementById("root"))

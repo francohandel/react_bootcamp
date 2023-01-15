1e0fd890b858591dc27eed9e61ed5da34a84e2d5
francohandel <francohandel@gmail.com>
2023-01-15 23:11:21 +0800

    styling

--- slot_machines/machine.js
+++ slot_machines/machine.js
@@ -2,15 +2,17 @@ class Machine extends React.Component {
   render() {
     const props = this.props
     let msg = ""
+    let status = false
     if (props.x == props.y && props.y == props.z && props.x == props.z) {
-      msg = <p>congrats yor are a lucky winner</p>
+      status = true
+      msg = "congrats yor are a lucky winner"
     } else {
-      msg = <p>sorry please try again!!!!</p>
+      msg = "sorry please try again!!!!"
     }
     return(
-      <div>
-        <h1>{props.x}-{props.y}-{props.z}</h1>
-        {msg}
+      <div className="Machine">
+        <h1 style= {{backgroundColor: 'purple', fontSize:'20px'}}>{props.x}-{props.y}-{props.z}</h1>
+        <p className={ status ? 'Machine-winner' : 'Machine-loser' }> {msg}</p>
       </div>
     )
   }

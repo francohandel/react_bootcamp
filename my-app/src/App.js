c0676a3be897bc3fb8a1c9414941ce539263fb20
francohandel <francohandel@gmail.com>
2023-01-17 14:24:38 +0800

    create react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Create App has been successfully completed</p>
         <a
           className="App-link"
           href="https://reactjs.org"

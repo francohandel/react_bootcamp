762a8b1d98bea8d36026f9aa4aa096967652c448
francohandel <francohandel@gmail.com>
2023-01-23 22:49:04 +0800

    adding Css

---
+++ my-app/src/components/Dog.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Dog.css'
+
+class Dog extends Component {
+  render() {
+    return (
+      <div className="Dog">
+        Hello, Dog
+      </div>
+    )
+  }
+}

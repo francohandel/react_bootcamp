f5658f89046f45acd68991ffce2665ab53b17162
francohandel <francohandel@gmail.com>
2023-01-29 22:36:30 +0800

    bug fix

--- my-app/src/components/Dog.js
+++ my-app/src/components/Dog.js
@@ -5,7 +5,7 @@ class Dog extends Component {
   render() {
     return (
       <div className="Dog">
-        This is a Sample Dog Component for css styling purposes
+        This is a Sample Dog Component
       </div>
     )
   }
